e53a9ed891d787fa071e02fb4bb22deacc894750
pzajas <zajas.piotr@gmail.com>
2023-08-24 21:17:48 +0200

    Install utilities.

---
+++ src/redux/store/store.ts
@@ -0,0 +1,25 @@
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import storage from 'redux-persist/lib/storage'
+
+import { configureStore } from '@reduxjs/toolkit'
+
+import rootReducer from '../reducers/rootReducer'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = configureStore({
+  reducer: persistedReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+export const persistor = persistStore(store)
